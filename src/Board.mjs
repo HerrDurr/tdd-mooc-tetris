2ffa5133177e0a0baebc45432f786fd70354f2ee
TUA <tua@polycon.fi>
2024-12-08 21:35:35 +0200

    ref: remove lineOne

--- src/Board.mjs
+++ src/Board.mjs
@@ -4,7 +4,6 @@ export class Board {
   width;
   height;
   lines = [emptyLine, emptyLine, emptyLine];
-  lineOne = emptyLine;
 
   constructor(width, height) {
     this.width = width;
@@ -19,14 +18,12 @@ export class Board {
     if (block === "Y") {
       throw new Error("already falling");
     } else {
-      this.lineOne = '.X.\n';
       this.lines[0] = '.X.\n';
     }
   }
 
   tick() {
     if (this.lines[1] === emptyLine) {
-      this.lineOne = emptyLine;
       this.lines[0] = emptyLine;
       this.lines[1] = '.X.\n';
     } else {
