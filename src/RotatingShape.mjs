418851f4035935dda31e2f2e5503e0a1e3b5ed72
TUA <tua@polycon.fi>
2024-12-15 22:06:13 +0200

    refactoring..

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -21,15 +21,19 @@ export class RotatingShape {
     return originalString.substring(0, index) + char + originalString.substring(index + 1);
   }
 
+  rotatedRow(oldColIdx) {
+    let newRow = '';
+    for (let oldRowIdx = 0; oldRowIdx < this.shapeString.length; oldRowIdx++) {
+      newRow = this.shapeString[oldRowIdx][oldColIdx] + newRow;
+    }
+    return newRow;
+  }
+
   rotateRight() {
     const newShape = Array.from(this.shapeString);
     for (let oldColIdx = 0; oldColIdx < this.shapeString.length; oldColIdx++) {
       let newRowIdx = oldColIdx;
-      for (let oldRowIdx = 0; oldRowIdx < this.shapeString.length; oldRowIdx++) {
-        let newColIdx = this.maxIndex() - oldRowIdx;
-        let newRow = this.replaceCharAtIndex(newShape[newRowIdx], this.shapeString[oldRowIdx][oldColIdx], newColIdx);
-        newShape[newRowIdx] = newRow;
-      }
+      newShape[newRowIdx] = this.rotatedRow(oldColIdx);
     }
     return new RotatingShape(newShape);
   }
